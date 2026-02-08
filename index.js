2c3c2e803bb5a8354ba987b1c636114173143326
Celine <obipraise2002@gmail.com>
2026-02-08 00:36:54 -0800

    Added response formatting to /calc route

--- index.js
+++ index.js
@@ -4,7 +4,48 @@ import calculator from "./calculator.js";
 const app = express();
 
 app.get("/calc", (req, res) => {
+
+  try {
   const { num1, num2, op } = req.query;
+
+  let result;
+  // call calculator functions
+  if (op === "add") {
+    result = calculator.add(num1, num2);
+  }
+  else if (op === "sub") {
+    result = calculator.subtract(num1, num2);
+  }
+  else if (op === "mul") {
+    result = calculator.multiply(num1, num2);
+  }
+  else if (op === "div") {
+    result = calculator.divide(num1, num2);
+  }
+  else {
+    throw new Error("Invalid operation");
+  }
+
+  //  RESPONDE FORMATTING
+  res.json({
+    success: true,
+    message: "Calculation successful",
+    data: {
+      num1,
+      num2,
+      operation: op,
+      result
+    }
+  });
+
+} catch (error) {
+  // ERROR RESPONSE FORMAT
+  res.json({
+    success: false,
+    message: error.message
+  });
+}
+
 });
 
 app.get("/", (req, res) => {
