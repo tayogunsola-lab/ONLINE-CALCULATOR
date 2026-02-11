a7e3a2337db1aca72cf2abfbc07b33eae2d4901e
tayogunsola-lab <tayogunsola@gmail.com>
2026-02-11 19:01:00 +0100

    Revert "chore: update ESLint rules (eqeqeq, no-console)"

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,14 @@ export default [
   js.configs.recommended,
   {
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
       globals: {
         console: "readonly",
         process: "readonly"
       }
     },
-
     rules: {
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "no-console": ["error", { allow: ["warn", "error"] }],
-      eqeqeq: "error",
-
-      "no-implicit-coercion": "warn"
+      "no-unused-vars": "warn",
+      "no-console": "error"
     }
   }
 ];
